2e4d58de539c9b657a3d0d1c5e62976889134030
Empika1 <dravidts1@gmail.com>
2024-08-11 22:00:27 -0400

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ezdrawing',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['ezdrawing'],
     install_requires=[
         'pygame>=2.6',
