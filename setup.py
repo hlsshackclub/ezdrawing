e1859f78d679ffc6a754bdbf5d1e63eef88165fc
Empika1 <dravidts1@gmail.com>
2024-08-11 22:10:39 -0400

    update version and gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ezdrawing',
-    version='0.0.2',
+    version='0.0.3',
     py_modules=['ezdrawing'],
     install_requires=[
         'pygame>=2.6',
